c82b1f35d65df969b90537d6e3d94f63884217cd
maze <ulyanow-kirill@mail.ru>
2026-02-24 11:18:38 +0300

    1

--- src/main/java/com/example/lab1fx/Starter.java
+++ src/main/java/com/example/lab1fx/Starter.java
@@ -11,7 +11,7 @@ public class Starter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("ConverterView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 500);
         stage.setTitle("MVC Converter");
         stage.setScene(scene);
         stage.show();
