1a358179069d0cf3a356fd7c26bf2aba18380b47
maze <ulyanow-kirill@mail.ru>
2026-02-24 08:24:42 +0300

    1

--- src/main/java/Model/ConverterModel.java
+++ src/main/java/Model/ConverterModel.java
@@ -2,56 +2,218 @@ package Model;
 
 public class ConverterModel
 {
-    public double convert(double value,String from, String to)
+    public String convert(String value,String from, String to)
     {
-        double result = 1;
-        double coeff = 1;
+        String result="";
 
-        if(from =="UNIT1")
+        if(from  == "Нота")
         {
-            if(to=="UNIT2")
+            if(to=="Гц")
             {
-                coeff = 2.54;
-                result = value*coeff;
+                switch(value)
+                {
+                    case "До":
+                        result = "261.63";
+                        break;
+                    case "Ре":
+                        result = "293.66";
+                        break;
+                    case "Ми":
+                        result = "329.63";
+                        break;
+                    case "Фа":
+                        result = "349.23";
+                        break;
+                    case "Соль":
+                        result = "392.00";
+                        break;
+                    case "Ля":
+                        result = "440.00";
+                        break;
+                    case "Си":
+                        result = "493.88";
+                        break;
+                    default:
+                        result = "Конвертация невозможна";
+                        break;
+                }
             }
-            else if(to=="UNIT3")
+            else if(to=="Длительность")
             {
-                coeff = 0.57;
-                result = value*coeff;
+                switch(value)
+                {
+                    case "До":
+                        result = "1";
+                        break;
+                    case "Ре":
+                        result = "1/4";
+                        break;
+                    case "Ми":
+                        result = "1/16";
+                        break;
+                    case "Фа":
+                        result = "1/32";
+                        break;
+                    case "Соль":
+                        result = "Длительность зависит от темпа и ритмического рисунка";
+                        break;
+                    case "Ля":
+                        result = "Длительность зависит от темпа и ритмического рисунка";
+                        break;
+                    case "Си":
+                        result = "Длительность зависит от темпа и ритмического рисунка";
+                        break;
+                    default:
+                        result = "Конвертация невозможна";
+                        break;
 
+                }
             }
-
+            else if(to=="Нота")
+            {
+                return value;
+            }
         }
-        else if(from == "UNIT2")
+        else if (from=="Гц")
         {
-            if(to=="UNIT1")
+            if(to=="Нота")
             {
-                coeff = 1.25;
-                result = value/coeff;
+                switch(value)
+                {
+                    case "261.63":
+                        result = "До";
+                        break;
+                    case "293.66":
+                        result = "Ре";
+                        break;
+                    case "329.63":
+                        result = "Ми";
+                        break;
+                    case "349.23":
+                        result = "Фа";
+                        break;
+                    case "392.00":
+                        result = "Соль";
+                        break;
+                    case "440.00":
+                        result = "Ля";
+                        break;
+                    case "493.88":
+                        result = "Си";
+                        break;
+                    default:
+                        result = "Конвертация невозможна";
+                        break;
+
+
+                }
             }
-            else if(to=="UNIT3")
+            else if(to=="Длительность")
             {
-                coeff = 0.14;
-                result = value*coeff;
+                switch(value)
+                {
+                    case "261.63":
+                        result = "1";
+                        break;
+                    case "293.66":
+                        result = "1/4";
+                        break;
+                    case "329.63":
+                        result = "1/16";
+                        break;
+                    case "349.23":
+                        result = "1/32";
+                        break;
+                    case "392.00":
+                        result = "Длительность зависит от темпа и ритмического рисунка";
+                        break;
+                    case "440.00":
+                        result = "Длительность зависит от темпа и ритмического рисунка";
+                        break;
+                    case "493.88":
+                        result = "Длительность зависит от темпа и ритмического рисунка";
+                        break;
+                    default:
+                        result = "Конвертация невозможна";
+                        break;
+                }
+            }
+            else if(to=="Гц")
+            {
+                return value;
             }
         }
-        else if(from == "UNIT3")
+        else if(from=="Длительность")
         {
-            if(to=="UNIT1")
+            if(to=="Нота")
             {
-                coeff = 0.57;
-                result = value/coeff;
+                switch(value)
+                {
+                    case "1":
+                        result = "До";
+                        break;
+                    case "1/4":
+                        result = "Ре";
+                        break;
+                    case "1/16":
+                        result = "Ми";
+                        break;
+                    case "1/32":
+                        result = "Фа";
+                        break;
+                    default:
+                        result = "Конвертация невозможна";
+                        break;
+                }
             }
-            else if(to=="UNIT2")
+            else if(to=="Гц")
             {
-                coeff = 0.14;
-                result = value/coeff;
+                switch(value)
+                {
+                    case "1":
+                        result = "261.63";
+                        break;
+                    case "1/4":
+                        result = "293.66";
+                        break;
+                    case "1/16":
+                        result = "329.63";
+                        break;
+                    case "1/32":
+                        result = "349.23";
+                        break;
+                    default:
+                        result = "Конвертация невозможна";
+                        break;
+                }
+            }
+            else if(to=="Длительность")
+            {
+                return value;
             }
         }
 
         return result;
 
     }
+
+//    public void main()
+//    {
+//        ConverterModel model = new ConverterModel();
+//
+//        // Тест 1: 10 своих единиц  = 25.4 чужих единиц
+//        double result1 = model.convert(10, "UNIT1", "UNIT2");
+//        System.out.println("10 своих единиц = " + result1 + " чужих единиц");
+//
+//        // Тест 2: 100 чужих единиц = ? своих единиц
+//        double result2 = model.convert(100, "UNIT2", "UNIT1");
+//        System.out.println("100 своих единиц = " + result2 + " чужих единиц");
+//
+//        // Проверьте, что результаты соответствуют ожиданиям
+//        if (Math.abs(result1 - 25.4) < 0.1) {
+//            System.out.println("✅ Тест 1 пройден!!");
+//        } else {
+//            System.out.println("❌ Тест 1 не пройден!!");
+//        }
+//    }
 }
-
-
--- src/main/java/com/example/lab1fx/ConverterController.java
+++ src/main/java/com/example/lab1fx/ConverterController.java
@@ -2,16 +2,73 @@ package com.example.lab1fx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+//import javax.swing.*;
+import Model.*;
 
 public class ConverterController {
+
     @FXML
     protected TextField txtFld1;
+    @FXML
+    protected TextField resultField;
+    @FXML
+    protected RadioButton fromUn1;
+    @FXML
+    protected RadioButton fromUn2;
+    @FXML
+    protected RadioButton fromUn3;
+    @FXML
+    protected RadioButton toUn1;
+    @FXML
+    protected RadioButton toUn2;
+    @FXML
+    protected RadioButton toUn3;
 
 
 
     @FXML
-    protected void convert() {
+    protected void handleConvert() {
+
+        ConverterModel model = new ConverterModel();
+        String unitFrom = "";
+        String unitTo = "";
+
+
+        if(fromUn1.isSelected())
+        {
+            unitFrom="Нота";
+        }
+        else if(fromUn2.isSelected())
+        {
+            unitFrom="Гц";
+        }
+        else if(fromUn3.isSelected())
+        {
+            unitFrom="Длительность";
+        }
+
+        if(toUn1.isSelected())
+        {
+            unitTo="Нота";
+        }
+        else if(toUn2.isSelected())
+        {
+            unitTo="Гц";
+        }
+        else if(toUn3.isSelected())
+        {
+            unitTo="Длительность";
+        }
+
+        String value = String.valueOf(txtFld1.getText());
+
+        String result = model.convert(value,unitFrom,unitTo);
+
+        resultField.setText(result);
 
 
 
